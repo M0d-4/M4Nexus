6ea4df96eb5bae785b3da297f183107522493019
Mod4 <omarelfarok24135@gmail.com>
2026-08-26 14:31:54 +0400

    fix build error

--- arch/arm64/include/asm/cpucaps.h
+++ arch/arm64/include/asm/cpucaps.h
@@ -58,8 +58,10 @@
 #define ARM64_WORKAROUND_1742098		37
 #define ARM64_HAS_SB				38
 #define ARM64_WORKAROUND_SPECULATIVE_SSBS	39
+#define ARM64_WORKAROUND_1188873		40
+#define ARM64_WORKAROUND_1542418		41
 
-/* kabi: reserve 40 - 62 for future cpu capabilities */
+/* kabi: reserve 42 - 62 for future cpu capabilities */
 #define ARM64_NCAPS				62
 
 #endif /* __ASM_CPUCAPS_H */
